1cbb9adde37042d355b3388c4a40a826d4adb2e5
Umer <hamzali4642@gmail.com>
2022-10-22 20:05:08 +0500

    Initial commit

---
+++ android/app/src/main/java/com/example/tonaldtesttask/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.tonaldtesttask;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/state/home_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+
+class HomeController extends GetxController{
+  var batteryPercentage = 6;
+
+  var isConnected = false.obs;
+}
